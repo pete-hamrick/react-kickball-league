1b9ebf895cc9aebb26133c4e1fb6117e40328d4f
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-06 18:20:22 -0800

    added comments to app.js planning components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg'
 import React from 'react'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <header className="App-header">{/* 3 links: home, teams, players */}</header>
+      {/* switch so it's 1 page */}
+      {/* home component for route '/' */}
+      {/* TeamList component for route '/teams' */}
+      {/* TeamDetail component for route '/teams/:teamId' */}
+      {/* PlayerList component for route '/players' */}
+      {/* PlayerDetail component for route '/players/:playerId' */}
+    </main>
   )
 }
 
