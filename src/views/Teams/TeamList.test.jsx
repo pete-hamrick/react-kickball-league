bbe4438311bef1f843a6a3245e40dd7ca97da09d
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-08 09:57:24 -0800

    updated TeamList test to pass snapshot and findByText

--- src/views/Teams/TeamList.test.jsx
+++ src/views/Teams/TeamList.test.jsx
@@ -1,9 +1,22 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route } from 'react-router-dom'
 import TeamList from './TeamList'
 
 // this doesn't test much without mocking? just snapshots the loading screen
 
-it('should render a list of the teams', () => {
-  const { container } = render(<TeamList />)
+it('should render a list of the teams', async () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/teams']}>
+      <Route path="/teams">
+        <TeamList />
+      </Route>
+    </MemoryRouter>
+  )
+
+  screen.getByText('Loading teams...')
+
+  const acmeTeamName = await screen.findByText('Acme USA', { exact: false })
+
   expect(container).toMatchSnapshot()
+  expect(acmeTeamName).toBeInTheDocument()
 })
