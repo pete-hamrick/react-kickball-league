0cc73e049d44311fcca5c875f4efe42b710ee79d
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-09 11:05:35 -0800

    added state hooks and other hooks to UpdateTeam, wrote useEffect to load in team data

--- src/views/Teams/UpdateTeam.jsx
+++ src/views/Teams/UpdateTeam.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 import TeamForm from '../../components/Teams/TeamForm'
+import { getTeamById } from '../../services/teams'
 
 export default function UpdateTeam() {
+  const { id } = useParams()
+  const [name, setName] = useState('')
+  const [city, setCity] = useState('')
+  const [state, setState] = useState('')
+  const [loading, setLoading] = useState(true)
+  const history = useHistory()
+
+  useEffect(() => {
+    async function getTeam() {
+      const { name, city, state } = await getTeamById(id)
+      setName(name)
+      setCity(city)
+      setState(state)
+      setLoading(false)
+    }
+
+    getTeam()
+  }, [id])
+
+  if (loading) return <h1>Loading Team...</h1>
+
   return (
     <>
       <fieldset>
-        <legend>Update Team!</legend>
+        <legend>Update Team</legend>
         <TeamForm />
       </fieldset>
     </>
