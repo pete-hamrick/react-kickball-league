4bc3ce614f13dcf5012af74b326cb9cad12033ee
Pete Hamrick <pete.hamrick@gmail.com>
2021-12-08 16:30:04 -0800

    fleshed out form to have properly named and labeled inputs, added basic styling and imported styling to AddNewTeam component

--- src/components/Teams/TeamForm.jsx
+++ src/components/Teams/TeamForm.jsx
@@ -4,6 +4,17 @@ export default function TeamForm() {
   return (
     <form>
       <h1>Form is here!</h1>
+
+      <label htmlFor="name">Name:</label>
+      <input id="name" name="name" type="text" />
+
+      <label htmlFor="city">City:</label>
+      <input id="city" name="city" type="text" />
+
+      <label htmlFor="state">State:</label>
+      <input id="state" name="state" type="text" />
+
+      <input type="submit" aria-label="Add A Team" />
     </form>
   )
 }
--- src/views/Teams/AddNewTeam.jsx
+++ src/views/Teams/AddNewTeam.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import TeamForm from '../../components/Teams/TeamForm'
+import './AddNewTeam.css'
 
 export default function AddNewTeam() {
   return (
-    <section>
+    <section className="new-team-container">
       <TeamForm />
     </section>
   )
